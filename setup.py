3b506949d0d8e6c35cc4bf0b0e7825a0fc1900d1
Anto Idicherian Lonappan <antolonappan@icloud.com>
2024-08-10 05:59:34 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 import setuptools
-from numpy.distutils.core import setup
-from numpy.distutils.misc_util import Configuration
+from setuptools import setup, Extension
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-def configuration(parent_package='', top_path=''):
-    config = Configuration('', parent_package, top_path)
-    # try extra_link_args = ['-lgomp', '-qopenmp'] was useful at some point for intel compilers (Kareem)
-    config.add_extension('plancklens.wigners.wigners', ['plancklens/wigners/wigners.f90'],
-                         extra_link_args=['-lgomp'], libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w'])
-    config.add_extension('plancklens.n1.n1f', ['plancklens/n1/n1f.f90'],
-                         extra_link_args=['-lgomp'], libraries=['gomp'], extra_f90_compile_args=['-fopenmp', '-w'])
-    return config
+# Define the Fortran extensions
+extensions = [
+    Extension(
+        name='plancklens.wigners.wigners',
+        sources=['plancklens/wigners/wigners.f90'],
+        extra_link_args=['-lgomp'],
+        libraries=['gomp'],
+        extra_f90_compile_args=['-fopenmp', '-w'],
+    ),
+    Extension(
+        name='plancklens.n1.n1f',
+        sources=['plancklens/n1/n1f.f90'],
+        extra_link_args=['-lgomp'],
+        libraries=['gomp'],
+        extra_f90_compile_args=['-fopenmp', '-w'],
+    )
+]
 
 setup(
     name='plancklens',
@@ -27,8 +34,8 @@ setup(
     author='Julien Carron',
     author_email='to.jcarron@gmail.com',
     description='Planck lensing python pipeline',
-    install_requires=['numpy', 'healpy', 'six'], #removed mpi4py for travis tests
+    install_requires=['numpy', 'healpy', 'six'],  # removed mpi4py for travis tests
     requires=['numpy', 'healpy', 'six', 'scipy'],
     long_description=long_description,
-    configuration=configuration)
-
+    ext_modules=extensions,
+)
